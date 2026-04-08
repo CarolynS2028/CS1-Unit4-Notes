65ccec998882cabb66f62878856edfbb3b9bbbd3
carolyn <carolyn.sydneus2028@gbwl.org>
2026-04-08 15:45:43 +0000

    variables

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
-console.log("hello world");
+// This is a single-line comment !
+
+/* This is a multi-line comment !
+The code below is an example statement (instruction/command)
+statements end with a semicolon ";"
+*/
+console.log("Welcome to JS"); 
+
+/* A variable is a named storage container for data (information) 
+*/
+
+
+// Declare a new variable (make space, give it a name/label)
+let username; 
+
+// Assign a value (data) to the var 
+username = "carolyn.s";
+
+// see the value stored in the var 
+console.log(username); 
+
+// declare + assogn in one step
+let luckyNum = 13;
